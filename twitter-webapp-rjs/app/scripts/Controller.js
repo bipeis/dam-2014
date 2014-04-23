f170277214371515b36983aa9ea7e593cc8d50f8
bipeis <bipeis@gmail.com>
2014-04-23 19:31:17 +0200

    ui

---
+++ twitter-webapp-rjs/app/scripts/Controller.js
@@ -0,0 +1,37 @@
+define('Controller',['Data', 'Service','UI'],function(DB,srv,UI){
+    'use strict';
+
+    var getTweetsFromTwitter = function(success, error){
+        srv.getTweets({},function(data){
+            processTweets(data, function(tweets){
+                DB.addTweets(tweets, success, error);
+                UI.showTweetsList(tweets);
+            }, error);
+        }, error);
+    };
+
+    var processTweets = function(data, success, error){
+        var tweets=[];
+        var tweet;
+        if(data && data.statuses && data.statuses.length >0){
+            for (var i = data.statuses.length - 1; i >= 0; i--) {
+                tweet ={
+                    id : data.statuses[i].id_str,
+                    text : data.statuses[i].text,
+                    date : new Date(data.statuses[i].created_at),
+                    user : data.statuses[i].user.name,
+                    image : data.statuses[i].user.user_profile_image_url
+                };
+                tweets.push(tweet);
+            }
+        }
+        success(tweets);
+    };
+    var error = function(){
+
+    };
+
+    return{
+        getTweetsFromTwitter : getTweetsFromTwitter
+    };
+});
